f2eaa569077372decb3a9db25b45242c0e778118
orevans <ore2000@columbia.edu>
2016-01-15 09:07:02 -0500

    Compare against analytic solution

--- uncracked_rudnicki_p2p1.py
+++ uncracked_rudnicki_p2p1.py
@@ -1,26 +1,29 @@
 from dolfin import *
+import numpy as np
+from scipy.special import expi
 import matplotlib.pyplot as plt
 
-# parameters
+# Set parameters
 B = Constant(0.6)
 q = Constant(1.0)
 G = Constant(0.5)
 K = Constant(1.0)
+c = (3.*float(B)**2 + 3.*float(K) + 4.*float(G))/(3.*float(K) + 4.*float(G))
 
-# mesh + function spaces
+# Define mesh and function spaces
 mesh = Mesh("thick_quarter_cylinder.xml")
 U = VectorFunctionSpace(mesh, "CG", 2)
 P = FunctionSpace(mesh, "CG", 1)
 M = MixedFunctionSpace([U, P])
-#b_parts = MeshFunction("size_t", mesh, mesh.topology().dim()-1)
 
+# Define boundaries
 class Outer(SubDomain):
     def inside(self, x, on_boundary):
         return near((x[0]*x[0] + x[1]*x[1]), 8.5*8.5)
 
-class Inne(SubDomain):
+class Inner(SubDomain):
     def inside(self, x, on_boundary):
-        return near((x[0]*x[0] + x[1]*x[1]), .1*.1)
+        return near((x[0]*x[0] + x[1]*x[1]), .01*.01)
 
 class XAxis(SubDomain):
     def inside(self, x, on_boundary):
@@ -30,8 +33,9 @@ class YAxis(SubDomain):
     def inside(self, x, on_boundary):
         return near(x[0], 0.)
 
+# Mark subdomains
 out = Outer()
-inn = Inne()
+inn = Inner()
 xaxis = XAxis()
 yaxis = YAxis()
 
@@ -42,66 +46,81 @@ inn.mark(boundaries, 2)
 xaxis.mark(boundaries, 3)
 yaxis.mark(boundaries, 4)
 
-# bcs
+# Dirichlet boundary conditions
 bc_x = DirichletBC(M.sub(0).sub(0), 0., boundaries, 3)
 bc_y = DirichletBC(M.sub(0).sub(1), 0., boundaries, 4)
 bcs  = [bc_x, bc_y]
 ds = Measure("ds")[boundaries]
 
-# ics
+# Initial conditions
 u_init = Constant((0.0, 0.0))
 p_init = Constant(0.0)
 u_prev = project(u_init, U)
 p_prev = interpolate(p_init, P)
 
-# trial + test functions
+# Define trial and test functions
 dv = TrialFunction(M)
 (u_t, p_t) = TestFunctions(M)
 vfunc = Function(M)
 u, p = split(vfunc)
 
-# time
-t = 0
+# Time-step
+t = 0.0
 dt = 0.1
-T = 100*dt
+T = 2*dt
 
-f = File("pressure.pvd")
+# Visualize solution
+f  = File("pressure.pvd")
+fe = File("pressure_exact.pvd")
 
-# stress
-#epsilon = sym(grad(u))
-#epsilond = epsilon - tr(epsilon)/3.*Identity(2)
-#sigma = 2.*G*epsilond + K*tr(epsilon)*Identity(2)
-#epsilon_t = sym(grad(u_t))
+# Analytic solution
+p_e = Function(P)
+n = P.dim()
+d = mesh.geometry().dim()
+p_e_values = np.zeros(n)
 
+dof_coords = P.dofmap().tabulate_all_coordinates(mesh)
+dof_coords.resize((n,d))
+dof_x = dof_coords[:,0]
+dof_y = dof_coords[:,1]
 
 while(t < T):
+    # Update analytic solution
+    p_e_values = -(1./(4.*np.pi))*expi(-c*(dof_x**2 + dof_y**2)/(4.*t))
+    p_e.vector()[:] = p_e_values
+
+    # Theta-step
     theta = Constant(0.5)
     ptheta = theta*p + (1. - theta)*p_prev
     utheta = theta*u + (1. - theta)*u_prev
 
-#    ds_in = ds
-
+    # Define stress tensor
     epsilon = sym(grad(u))
     epsilond = epsilon - tr(epsilon)/3.*Identity(2)
     sigma = 2.*G*epsilond + K*tr(epsilon)*Identity(2)
     epsilon_t = sym(grad(u_t))
 
+    # Define facet normal and radius
     n = FacetNormal(mesh)
-    r = Expression("x[0]*x[0] + x[1]*x[1]")
+    r = Expression("pow(x[0]*x[0] + x[1]*x[1], 0.5)")
 
+    # Weak form
     F = inner(sym(grad(u_t)), sigma)*dx - B*div(u_t)*p*dx + inner(u_t, n)*p*ds(1) + inner(u_t, n)*p*ds(2) 
-    F += p_t*B*div(u - u_prev)*dx + p_t*(p - p_prev)*dx + dt*inner(grad(p_t), grad(ptheta))*dx - dt*p_t*(5./r)*ds(0) #- dt*p_t*(5./r)*ds(0)
+    F += p_t*B*div(u - u_prev)*dx + p_t*(p - p_prev)*dx + dt*inner(grad(p_t), grad(ptheta))*dx - dt*p_t/2./3.1415926538/r*ds(0) 
 
     # solve
     J = derivative(F, vfunc, dv)
     solve(F == 0, vfunc, bcs=bcs, J=J, solver_parameters={"newton_solver": {'absolute_tolerance': 1E-8, 'relative_tolerance': 1E-8, 'maximum_iterations': 25, 'relaxation_parameter': 1.0}})
 
-#    solve(F == 0, vfunc, bcs=bcs, J=J, solver_parameters={"nonlinear_solver": "snes"})
-
-    f << vfunc.sub(1)
-
     u, p = split(vfunc)
+
+    # Print error (pressure)
+    print "L2 error: %s" % assemble(inner(p - p_e, p - p_e)*dx)
+
     assign(u_prev, vfunc.sub(0))
     assign(p_prev, vfunc.sub(1))
-
     t += dt
+
+f  << vfunc.sub(1)
+fe << p_e
+
